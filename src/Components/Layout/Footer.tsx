38445f4950e7b58f352fafec3ede5724debe185c
Marcin2 <marcinjunka@gmail.com>
2023-06-24 11:34:31 +0200

    footerfix

--- src/Components/Layout/Footer.tsx
+++ src/Components/Layout/Footer.tsx
@@ -80,12 +80,12 @@ function Footer() {
             <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
               {/* <!-- Links --> */}
               <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
-              <p><i className="fas fa-home me-3"></i> Karlskoga, Sandtorpsvägen 3A</p>
+              <p><i className="fas fa-home me-3" style={{width:"16px"}}></i>Karlskoga, Sweden</p>
               <p>
                 <i className="fas fa-envelope me-3"></i>
-                kkmuk87@gmail.com
+                Kkkk87@gmail.com
               </p>
-              <p><i className="fas fa-phone me-3"></i> 072 833 42 26</p>
+              <p><i className="fas fa-phone me-3"></i>072 833 42 26</p>
               {/* <p><i className="fas fa-print me-3"></i> + 01 234 567 89</p> */}
             </div>
             {/* <!-- Grid column --> */}
